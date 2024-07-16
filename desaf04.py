a3ce4ae67e7a38afb35fd6019c411f2757f8f4d2
Lucas <lucascasotti1@gmail.com>
2024-07-16 12:36:10 -0300

    Novo arquivo

---
+++ desaf04.py
@@ -0,0 +1,21 @@
+print('Gerador de PA')
+print('-=' * 10)
+
+primeiro = int(input('Digite um número: ')) 
+razao = int(input('Digite a razão: ')) 
+termo = primeiro 
+cont = 1
+total = 0
+mais = 10
+
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print(f'{termo}', end= ' ')
+        termo += razao
+        cont += 1 
+    print('PAUSA') 
+    mais = int(input('Quantos termos você quer mostrar a mais? ')) 
+print('FIM')
+
+    
